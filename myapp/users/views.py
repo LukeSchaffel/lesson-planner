07ee26c04413c74726087a933a2b5a02f491a4f1
LukeSchaffel <lukeschaffel@gmail.com>
2022-04-13 15:47:32 -0400

    try

--- myapp/users/views.py
+++ myapp/users/views.py
@@ -30,7 +30,7 @@ def students(username):
   page = request.args.get('page', 1, type=int)
   user = User.query.filter_by(username=username).first_or_404()
   lessons = Lesson.query.filter_by(author=user).distinct('student').paginate(page=page, per_page=5) 
-  return render_template('students.html', lessons=lessons, user=user, username=username)  
+  return render_template('students.html' lessons=lessons, user=user, username=username)  
 
 
 
