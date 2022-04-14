53c7ddfa9a165eb66321a6105ccee66c6126e716
LukeSchaffel <lukeschaffel@gmail.com>
2022-04-14 13:03:23 -0400

    style

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,12 +1,13 @@
 # core/views.py 
 
 from flask import render_template, request, Blueprint
+from myapp.models import User
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    
+
     return render_template('index.html')
 
 @core.route('/info')
